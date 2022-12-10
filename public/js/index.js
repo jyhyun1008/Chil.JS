450fe6c50bbd0c5d33384d50c6a36fe257e35322
root <hyun1008@kasi.re.kr>
2022-12-10 21:18:58 +0000

    중간 저장

--- public/js/index.js
+++ public/js/index.js
@@ -2,26 +2,21 @@
 
 function almanacEnter() {
     if (window.event.keyCode == 13) {
-        var year = document.getElementById("yearInput").value
-        const req = {
-            newYear: year
-        }
-        fetch('/almanac', {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(req)
-        }).then(function(res){
-            window.location.href = res.url;
-            return res;
-        })
+        almanacClick()
     }
 }
 
 function calendarEnter() {
     if (window.event.keyCode == 13) {
-        var year = document.getElementById("yearInput").nodeValue
+        calendarClick()
+    }
+}
+
+function calendarClick() {
+    var year = document.getElementById("yearInput").value
+    if (year < 1281 || year > 1654){
+        alert("This year is not supported by the site.")
+    } else {
         const req = {
             newYear: year
         }
@@ -38,36 +33,23 @@ function calendarEnter() {
     }
 }
 
-function calendarClick() {
-    var year = document.getElementById("yearInput").value
-    const req = {
-        newYear: year
-    }
-    fetch('/calendar', {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(req)
-    }).then(function(res){
-        window.location.href = res.url;
-        return res;
-    })
-}
-
 function almanacClick() {
     var year = document.getElementById("yearInput").value
-    const req = {
-        newYear: year
+    if (year < 1281 || year > 1654){
+        alert("This year is not supported by the site.")
+    } else {
+        const req = {
+            newYear: year
+        }
+        fetch('/almanac', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(req)
+        }).then(function(res){
+            window.location.href = res.url;
+            return res;
+        })
     }
-    fetch('/almanac', {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(req)
-    }).then(function(res){
-        window.location.href = res.url;
-        return res;
-    })
 }
